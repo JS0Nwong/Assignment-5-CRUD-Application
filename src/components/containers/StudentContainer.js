e123c8099a8ad575a6cec99e4e608d21da5d6880
xiqian <keviny413@gmail.com>
2022-07-11 22:45:49 -0400

    added delete function for studentview

--- src/components/containers/StudentContainer.js
+++ src/components/containers/StudentContainer.js
@@ -7,7 +7,7 @@ If needed, it also defines the component's "connect" function.
 ================================================== */
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchStudentThunk } from "../../store/thunks";
+import { fetchStudentThunk, deleteStudentThunk } from "../../store/thunks";
 import { StudentView } from "../views";
 
 class StudentContainer extends Component {
@@ -21,7 +21,10 @@ class StudentContainer extends Component {
   render() {
     return (
       <div>
-        <StudentView student={this.props.student} />
+        <StudentView
+        student={this.props.student} 
+        deleteStudent={this.props.deleteStudent}
+        />
       </div>
     );
   }
@@ -39,6 +42,7 @@ const mapState = (state) => {
 const mapDispatch = (dispatch) => {
   return {
     fetchStudent: (id) => dispatch(fetchStudentThunk(id)),
+    deleteStudent: (studentId) => dispatch(deleteStudentThunk(studentId)),
   };
 };
 
